1174f11a2a203975697a6e64fe8aaf97332e6d7f
Pablo Vargas <pvargas.figueroa@gmail.com>
2017-11-29 09:44:04 -0300

    se agrega producto

---
+++ src/app/components/producto/producto.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producto',
+  templateUrl: './producto.component.html'
+})
+export class ProductoComponent{
+
+  constructor() { }
+
+
+}
