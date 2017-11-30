0cb9dbdf6c55125bdc322226499c733004423139
Pablo Vargas <pvargas.figueroa@gmail.com>
2017-11-30 11:40:18 -0300

    servicio de firebase integrado

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,13 +5,34 @@ import { Http } from '@angular/http';
 export class InformacionService {
 
   info:any = {};
+  cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[] = [];
+
   constructor( public http:Http ) {
 
-    this.http.get("assets/data/info.paginas.json")
+    this.carga_info();
+    this.carga_sobre_nosotros();
+
+  }
+
+  public carga_info(){
+     this.http.get("assets/data/info.paginas.json")
              .subscribe( data =>{
-               console.debug(data.json());
+              // console.debug(data.json());
+               this.cargada = true;
                this.info = data.json();
              })
+  }
+  public carga_sobre_nosotros(){
+
+       this.http.get("https://bremsen-d8cc6.firebaseio.com/equipo.json")
+             .subscribe( data =>{
+              console.debug(data.json());
+              this.cargada_sobre_nosotros = true;
+               this.equipo = data.json();
+             })
+
   }
 
 }
