39aa2e9d003d034627e894a313314a3de6ed314b
Pablo Vargas <pvargas.figueroa@gmail.com>
2017-12-01 17:56:52 -0300

    servicio productos

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class ProductosService {
+
+  productos:any[] = [];
+  constructor( private http:Http) {
+
+      this.cargar_productos();
+
+  }
+
+  public cargar_productos(){
+
+        this.http.get('https://bremsen-d8cc6.firebaseio.com/productos_idx.json').subscribe(
+          res => {
+              console.debug(res.json());
+          }
+        )
+
+
+  }
+}
