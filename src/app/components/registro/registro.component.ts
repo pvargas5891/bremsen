3a995bc83712d753892520f57cc20ac8d89c0bb0
pablo vargas <pvargas.figueroa@gmail.com>
2017-12-05 00:37:08 -0300

    llenado de campo region

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { InformacionService } from '../../services/informacion.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html'
 })
 export class RegistroComponent {
-
-  constructor() { }
-
+  public regionSeleccion: any[] = [];
+  constructor(public _is: InformacionService) {
+  
+   }
+  public cambiaCiudad(event){
+    console.debug(this.regionSeleccion);
+  }
 }
--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -8,43 +8,45 @@ export class InformacionService {
   cargada:boolean = false;
   cargada_sobre_nosotros:boolean = false;
   equipo:any[] = [];
-  regiones:any[] = [];
+  public regiones: any[] = [];
+  public ciudades: any[] = [];
+  public comunas:any[] = [];
   constructor( public http:Http ) {
 
-    this.carga_info();
-    this.carga_sobre_nosotros();
     this.getRegiones();
 
   }
 
-  public carga_info(){
-     /*this.http.get("assets/data/info.paginas.json")
-             .subscribe( data =>{
-              // console.debug(data.json());
-               this.cargada = true;
-               this.info = data.json();
-             })*/
-  }
-  public getRegiones(){
+  public getCiudades(ciudad){
     
-    this.http.get("http://bremsen.kodamas.cl/maqueta/location.php")
+    this.http.get("http://bremsen.kodamas.cl/maqueta/location.php?ciudad=" + ciudad)
              .subscribe( data =>{
-              console.debug(data.json());
+              //console.debug(data.json());
                this.cargada = true;
-               this.regiones = data.json();
-               console.debug(this.regiones[0][0]);
-               console.debug(this.regiones[0][1]);
+               this.ciudades = data.json();
              })
   }
-  public carga_sobre_nosotros(){
+  public getComunas(comuna) {
 
-      /* this.http.get("https://bremsen-d8cc6.firebaseio.com/equipo.json")
-             .subscribe( data =>{
-              console.debug(data.json());
-              this.cargada_sobre_nosotros = true;
-               this.equipo = data.json();
-             })*/
+    this.http.get("http://bremsen.kodamas.cl/maqueta/location.php?comuna" + comuna)
+      .subscribe(data => {
+        //console.debug(data.json());
+        this.cargada = true;
+        this.comunas = data.json();
+        // console.debug(this.regiones[0][0]);
+        //console.debug(this.regiones[0][1]);
+      })
+  }
+  public getRegiones() {
 
+    this.http.get("http://bremsen.kodamas.cl/maqueta/location.php")
+      .subscribe(data => {
+        //console.debug(data.json());
+        this.cargada = true;
+        this.regiones = data.json();
+        // console.debug(this.regiones[0][0]);
+        //console.debug(this.regiones[0][1]);
+      })
   }
 
 }
